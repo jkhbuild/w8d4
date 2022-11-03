7a8727b20c484334aaa97761233f22ee90a35bd9
jkhbuild <jkh.build@gmail.com>
2022-11-03 15:29:36 -0400

    myBind completed

--- 02_addNumbers.js
+++ 02_addNumbers.js
@@ -7,11 +7,11 @@ const rl = readline.createInterface({
 function addNumbers(sum, numsLeft, completionCallback) {
   if (numsLeft > 0) {
     rl.question("Provide a number:", (response) => {
-      let number = parseInt(response);
-      sum += number;
-      numsLeft--;
-      console.log(sum);
-      addNumbers(sum, numsLeft, completionCallback);
+      // let number = parseInt(response);
+      // sum += number;
+      // numsLeft--;
+      // console.log(sum);
+      // addNumbers(sum, numsLeft, completionCallback);
     });
   } else {
     completionCallback(sum);
